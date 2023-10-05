39fc3a3ea9b31e7f192717fdd4a2dbeb99bfa7e7
Timofei Kurohtin <forgwent@gmail.com>
2023-10-05 23:37:13 +0300

    Add storybook & Create button

---
+++ src/components/Button/types.ts
@@ -0,0 +1,11 @@
+export type ButtonColor =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "error"
+  | "info"
+  | "warning";
+
+export type ButtonVariant = "filled" | "outlined" | "text";
+
+export type ButtonSize = "small" | "medium" | "large";
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#cad9ff",
+      secondary: "#dee2ff",
+      error: "#FAD2D2",
+      success: "#E3F9DD",
+      warning: "#FFFDD7",
+      info: "#cad9ff",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
